e87402d094440e43322188eb13ad667f06bb5ab3
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-03-31 18:30:35 +0800

    lesson 6 assignment 6h finished

--- 06/react-course-afterAssignment/ecommerce-project/src/App.jsx
+++ 06/react-course-afterAssignment/ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import CheckOutPage from "./pages/checkout/CheckOutPage";
 import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 import "./App.css";
 
 function App() {
@@ -18,6 +19,10 @@ function App() {
             <Route
                 path="orders"
                 element={<OrdersPage />}
+            />
+            <Route
+                path="tracking"
+                element={<TrackingPage />}
             />
         </Routes>
     );
