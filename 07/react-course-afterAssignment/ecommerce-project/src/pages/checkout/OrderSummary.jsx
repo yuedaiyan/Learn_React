c3bbebc6937ab1492165c9acc7326fc3b3d5ae65
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-08 13:14:39 +0800

    lesson 7 assignment 7b finished

--- 07/react-course-afterAssignment/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ 07/react-course-afterAssignment/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
-import formatMoney from "../../utils/money";
 import DeliveryOptions from "./DeliveryOptions";
+import CartItemDetials from "./CartItemDetails";
 
 function OrderSummary({ deliveryOptions, cart }) {
     return (
@@ -19,22 +19,9 @@ function OrderSummary({ deliveryOptions, cart }) {
                             <div className="delivery-date">Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format("dddd, MMMM D")}</div>
 
                             <div className="cart-item-details-grid">
-                                <img
-                                    className="product-image"
-                                    src={cartItem.product.image}
-                                />
 
-                                <div className="cart-item-details">
-                                    <div className="product-name">{cartItem.product.name}</div>
-                                    <div className="product-price">{formatMoney(cartItem.product.priceCents)}</div>
-                                    <div className="product-quantity">
-                                        <span>
-                                            Quantity: <span className="quantity-label">2</span>
-                                        </span>
-                                        <span className="update-quantity-link link-primary">Update</span>
-                                        <span className="delete-quantity-link link-primary">Delete</span>
-                                    </div>
-                                </div>
+                                <CartItemDetials cartItem={cartItem} />
+
                                 <DeliveryOptions
                                     cartItem={cartItem}
                                     deliveryOptions={deliveryOptions}
