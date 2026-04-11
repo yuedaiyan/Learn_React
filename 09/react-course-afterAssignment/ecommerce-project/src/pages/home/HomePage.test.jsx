518efadd347a98036f4aabf06a44244513df93b3
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-11 18:56:38 +0800

    lesson 9 assignment: 9g finished

--- 09/react-course-afterAssignment/ecommerce-project/src/pages/home/HomePage.test.jsx
+++ 09/react-course-afterAssignment/ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -68,4 +68,40 @@ describe("HomePage component", () => {
         expect(within(productContainers[0]).getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")).toBeInTheDocument();
         expect(within(productContainers[1]).getByText("Intermediate Size Basketball")).toBeInTheDocument();
     });
+
+    it("Add to Cart buttons work", async () => {
+        render(
+            // 因为Homepage里面有Header,Header里面有Link,所以必须使用Router包裹
+            <MemoryRouter>
+                <HomePage
+                    cart={[]}
+                    loadCart={loadCart}
+                />
+                ,
+            </MemoryRouter>,
+        );
+        const user = userEvent.setup();
+
+        const productContainers = await screen.findAllByTestId("product-container");
+
+        // 点击:第一个商品 Add to Cart
+        const addToCartbutton0 = within(productContainers[0]).getByTestId("add-to-cart-button");
+        await user.click(addToCartbutton0);
+        // 点击:第二个商品 Add to Cart
+        const addToCartbutton1 = within(productContainers[1]).getByTestId("add-to-cart-button");
+        await user.click(addToCartbutton1);
+
+        // 检测两次发送的数据是否正确
+        expect(axios.post).toHaveBeenNthCalledWith(1, "/api/cart-items", {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 1,
+        });
+        expect(axios.post).toHaveBeenNthCalledWith(2, "/api/cart-items", {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+        });
+        
+        // 检测是不是更新了两次购物车
+        expect(loadCart).toHaveBeenCalledTimes(2)
+    });
 });
