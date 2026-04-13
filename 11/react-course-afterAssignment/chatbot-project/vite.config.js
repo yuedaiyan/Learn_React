ce980beaec3d8f3cf45537ea63762461567e8ba5
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-13 17:49:39 +0800

    lesson 11 assignment: 11c finished

--- 11/react-course-afterAssignment/chatbot-project/vite.config.js
+++ 11/react-course-afterAssignment/chatbot-project/vite.config.js
@@ -3,9 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+            },
+        }),
+    ],
     server: {
         open: "/",
     },
-    base:'/chatbot-project'
+    base: "/chatbot-project",
 });
