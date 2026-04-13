6d55c2221decc8358b8fd514e6d44a84a88cb24e
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-13 17:15:37 +0800

    edit signature

--- 11/react-course-afterAssignment/ecommerce-project/src/components/Signature.tsx
+++ 11/react-course-afterAssignment/ecommerce-project/src/components/Signature.tsx
@@ -3,7 +3,7 @@ function Signature() {
     return (
         <>
             <div className="signature">
-                <div className="name">- Yue Dayan -</div>
+                <div className="name">- Yue Daiyan -</div>
                 <div className="time">2026 - 04 - 13</div>
             </div>
             <script
