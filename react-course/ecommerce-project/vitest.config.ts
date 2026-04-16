58042221872716d3e3579ff481220a08238958bb
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-16 23:41:21 +0800

    TypeScript: update setup files to ts

--- react-course/ecommerce-project/vitest.config.ts
+++ react-course/ecommerce-project/vitest.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     test: {
         environment: "jsdom",
         globals: true,
-        setupFiles: "./setupTests.js",
+        setupFiles: "./setupTests.ts",
     },
 });
